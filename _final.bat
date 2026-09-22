72a67896ad9600320f2b2ae7bb0706629c6a4a99
Julian Waits II <kuroninja@ckuro.cc>
2026-09-22 09:18:56 -0400

    drop helper scripts from tracking

--- _final.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d D:\dd2-forge
-node --check assets\app.js && echo JS_SYNTAX_OK
-if exist _cleanup.bat del _cleanup.bat
-if exist _final.bat (echo skip)
-git add -A
-git commit -q -m "tidy helper scripts"
-git push -q
-git log --oneline -1
